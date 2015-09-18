9093f2c4eac33fdb35c50cf6915ac54087d2e659
Shin Chin <shinchin136@gmail.com>
2015-09-17 20:11:10 -0400

    add painting at index view with search

--- app/controllers/paintings_controller.rb
+++ app/controllers/paintings_controller.rb
@@ -1,2 +1,9 @@
 class PaintingsController < ApplicationController
+  def index
+    if params[:q]
+      @paintings = Painting.search(params[:q])
+    else
+      @paintings = Painting.first(5)
+    end
+  end
 end
--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,4 +1,8 @@
 class Painting < ActiveRecord::Base
   belongs_to :museum
   belongs_to :artist
+
+  def self.search(query)
+    where('title LIKE :query', :query => "%#{query}%")
+  end
 end
