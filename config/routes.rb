8f48a24a8b5d0cdbc03a95e30fef20efea83a776
Shin Chin <shinchin136@gmail.com>
2015-09-17 20:19:32 -0400

    add MuseumsController and its routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'paintings#index'
   resources :paintings
+  resources :museums
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
